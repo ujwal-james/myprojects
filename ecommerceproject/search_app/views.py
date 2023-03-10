8fd7bfd608a024341b4f83f7fd0f0faa07b29cfe
ujwal-james <127179951+ujwal-james@users.noreply.github.com>
2023-03-10 16:34:40 +0530

    Update views.py

--- ecommerceproject/search_app/views.py
+++ ecommerceproject/search_app/views.py
@@ -9,4 +9,4 @@ def SearchResult(request):
     if 'q' in request.GET:
         query=request.GET.get('q')
         products=Product.objects.all().filter(Q(name__contains=query) | Q(description__contains=query))
-        return render(request,'search.html',{'query':query,'products':products})
+    return render(request,'search.html',{'query':query,'products':products})
